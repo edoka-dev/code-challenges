aeb5442f0350c6554b114edb67db67e31f80feaf
edoka-dev <ersadoka@outlook.com>
2024-10-04 12:39:31 -0400

    Migrating project repository to new Github account

---
+++ react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Here will stay the router
+    </div>
+  );
+}
+
+export default App;
